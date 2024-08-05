53952ed5ea3d81150dad7112570cebe7bd623f61
pisceskibo <taquangtung2003@gmail.com>
2024-08-05 14:31:12 +0700

    delete line

--- routers/function.py
+++ routers/function.py
@@ -11,7 +11,6 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-
 # Kiểm tra password
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def verify_password(plain_password, hashed_password):
